970addd4e3423b929df418ad80eaa03ae4ff67a3
Ralph Tamayo <ralph.mikel.tamayo@accenture.com>
2024-09-22 21:17:57 +0800

    Use Hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <ReduxProvider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ReduxProvider>
 );
