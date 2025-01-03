b2c18484704f6cdbc25a011b0f9becea2e0d8e57
Ralph Tamayo <ralph.mikel.tamayo@accenture.com>
2025-01-03 10:44:58 +0800

    Modify registry's content

--- src/pages/RegistryPage.tsx
+++ src/pages/RegistryPage.tsx
@@ -4,8 +4,9 @@ function RegistryPage() {
   return (
     <div className="main-container">
       <div className="main-desc">
-        We'll post our registry here when we're finished building it. Thank you
-        in advance!
+        Your presence at our wedding is the greatest gift we could ask for.
+        However, if you’d like to contribute to our future together, a monetary
+        gift would be appreciated.
       </div>
     </div>
   );
