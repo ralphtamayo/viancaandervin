de4d59c807ce1ab4062ce57363ec2f797fb4cc91
ralphtamayo <32214665+ralphtamayo@users.noreply.github.com>
2025-05-17 00:16:03 +0800

    May updates

--- src/pages/WelcomeDinnerPage.tsx
+++ src/pages/WelcomeDinnerPage.tsx
@@ -8,10 +8,16 @@ function WelcomeDinnerPage() {
     <div className="container">
       <div className="row">
         <div className={width <= 1024 ? "twelve columns" : "six columns"}>
-          <img src="/dinner-cover.jpeg" className="main-image first-image" />
+          <img
+            src="/dinner-cover.jpeg"
+            className="invitation-image first-image"
+          />
         </div>
         <div className={width <= 1024 ? "twelve columns" : "six columns"}>
-          <img src="/dinner-attire.jpeg" className="main-image second-image" />
+          <img
+            src="/dinner-attire.jpeg"
+            className="invitation-image second-image"
+          />
         </div>
       </div>
     </div>
